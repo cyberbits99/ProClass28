289935e7171e653b82be4ffcfedc3ac0d3c94e7a
cyberbits99 <79581339+cyberbits99@users.noreply.github.com>
2021-04-09 12:31:48 +0530

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -7,6 +7,7 @@ class Slingshot{
             stiffness: 0.04,
             length: 10
         }
+        this.pointB=pointB;
         this.Slingshot = Constraint.create(options);
         World.add(world, this.Slingshot);
     }
